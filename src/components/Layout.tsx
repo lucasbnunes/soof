3eb896d44374b5ba4184116e736455d8603db165
lucasbnunes <lucasbnunes23@gmail.com>
2023-02-26 15:05:08 -0300

    feat: add title, description and favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,19 @@
 import { Container } from "@mantine/core";
+import Head from "next/head";
 import { BackgroundImage } from "../features/background/BackgroundImage";
 
 export function Layout({ children }: React.PropsWithChildren) {
 
   return (
-    <Container fluid h="100vh" sx={{ height: "100vh", position: "relative" }}>
-      <BackgroundImage />
-      {children}
-    </Container>
+    <>
+      <Head>
+        <title>soof</title>
+      </Head>
+
+      <Container fluid h="100vh" sx={{ height: "100vh", position: "relative" }}>
+        <BackgroundImage />
+        {children}
+      </Container>
+    </>
   )
 }
